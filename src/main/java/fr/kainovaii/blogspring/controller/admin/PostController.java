ffdf4760a927b4d8fd4b5d11c504e6dcb8470c10
kainovaii <kainodev@proton.me>
2025-07-18 16:43:41 +0200

    👨‍💻 Dev (Up)

--- src/main/java/fr/kainovaii/blogspring/controller/admin/PostController.java
+++ src/main/java/fr/kainovaii/blogspring/controller/admin/PostController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("adminPostController")
@@ -20,6 +21,16 @@ public class PostController
     @GetMapping("")
     public String home(Model model) {
         model.addAttribute("posts", postService.findAll());
-        return "admin/posts";
+        return "admin/posts/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable Long id) {
+        return postService.findById(id)
+        .map(post -> {
+            model.addAttribute("post", post);
+            return "admin/posts/edit";
+        })
+        .orElse("error/404");
     }
 }
